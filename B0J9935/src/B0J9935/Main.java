dd512c6784a7790a0996594db8a6a62c08697913
Kim, Jinhue <ghty68752@naver.com>
2025-04-04 04:33:36 +0900

    B0J9935 해결

--- B0J9935/src/B0J9935/Main.java
+++ B0J9935/src/B0J9935/Main.java
@@ -8,13 +8,26 @@ public class Main {
 		Scanner scanner=new Scanner(System.in);
 		String str=scanner.nextLine();
 		String boom=scanner.nextLine();
+		scanner.close();
 		
-		while(Pattern.compile(boom).matcher(str).find()) {
-			str=str.replaceAll(boom, "");
-		}
-		if(str.isEmpty()) {
-			str="FRULA";
-		}
-		System.out.print(str);
-	}
+		StringBuilder sb = new StringBuilder();
+		
+		// 문자열을 하나씩 처리하면서 StringBuilder에 추가
+        for (char c : str.toCharArray()) {
+            sb.append(c);
+
+            // StringBuilder의 마지막 부분이 boom 문자열과 일치하는지 확인
+            if (sb.length() >= boom.length() && sb.substring(sb.length() - boom.length()).equals(boom)) {
+                // 일치하면 boom을 제거
+                sb.setLength(sb.length() - boom.length());
+            }
+        }
+
+        // 결과가 비어 있으면 "FRULA" 출력
+        if (sb.length() == 0) {
+            sb.append("FRULA");
+        }
+
+        System.out.print(sb);
+    }
 }

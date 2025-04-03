6e265e7e01961a53ebf5c491ecaa4fbbd496620f
Kim, Jinhue <ghty68752@naver.com>
2025-04-04 04:00:59 +0900

    3차 시도 실패

--- B0J9935/src/B0J9935/Main.java
+++ B0J9935/src/B0J9935/Main.java
@@ -1,14 +1,13 @@
 package B0J9935;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
-	public static void main(String[] args)throws Exception{
-		var reader=new BufferedReader(new InputStreamReader(System.in));
-		String str=reader.readLine();
-		String boom=reader.readLine();
+	public static void main(String[] args){
+		Scanner scanner=new Scanner(System.in);
+		String str=scanner.nextLine();
+		String boom=scanner.nextLine();
 		
 		while(Pattern.compile(boom).matcher(str).find()) {
 			str=str.replaceAll(boom, "");

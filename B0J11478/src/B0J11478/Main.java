3d7b3c893547ddab0c54d80b230c0d9fcac20c1d
Kim, Jinhue <ghty68752@naver.com>
2025-04-04 03:47:41 +0900

    B0J11479해결

---
+++ B0J11478/src/B0J11478/Main.java
@@ -0,0 +1,20 @@
+package B0J11478;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		var reader = new BufferedReader(new InputStreamReader(System.in));
+		String input = reader.readLine();
+		Set<String> set = new HashSet<>();
+		for (int j = 0; j < input.length()+1; j++) {
+			for (int i = j+1; i < input.length()+1; i++) {
+				set.add(input.substring(j, i));
+			}
+		}
+		System.out.print(set.size());
+	}
+}
